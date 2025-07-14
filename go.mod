6ad64044811d4eca88c254c8d5bbdbf7a2686d59
SamJohn04 <samuel.john.codes@gmail.com>
2025-07-14 19:42:24 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/SamJohn04/gocache
+
+go 1.22.8
---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+    "strings"
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+type requestResponse struct {
+    statusCode int
+    data string
+}
+
+func main() {
+    var port, server string
+    cache := make(map[string]requestResponse)
+
+    fmt.Print("Enter port and server to connect to: ")
+    fmt.Scanln(&port, &server)
+
+    port = fmt.Sprintf(":%v", port)
+
+    fmt.Println("Running on port ", port)
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        seek := r.URL.Path[1:]
+
+        if value, exists := cache[seek]; exists {
+            w.WriteHeader(value.statusCode)
+            io.Copy(w, strings.NewReader(value.data))
+        } else {
+            resp := forward(r, server, seek)
+            defer resp.Body.Close()
+
+            for k, vals := range resp.Header {
+                for _, v := range vals {
+                    w.Header().Add(k, v)
+                }
+            }
+
+            s, _ := io.ReadAll(resp.Body)
+            cache[seek] = requestResponse{
+                statusCode: resp.StatusCode,
+                data: string(s),
+            }
+
+            w.WriteHeader(resp.StatusCode)
+            io.Copy(w, resp.Body)
+        }
+    })
+
+    log.Fatal(http.ListenAndServe(port, nil))
+}
+
+func forward(r *http.Request, server string, seek string) *http.Response {
+    body, err := io.ReadAll(r.Body)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    defer r.Body.Close()
+
+    url := fmt.Sprintf("%v/%v", server, seek)
+
+    proxyRequest, err := http.NewRequest(r.Method, url, bytes.NewReader(body))
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    for h, vals := range r.Header {
+        for _, v := range vals {
+            proxyRequest.Header.Add(h, v)
+        }
+    }
+
+    client := &http.Client{}
+    resp, err := client.Do(proxyRequest)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    return resp
+}
+
